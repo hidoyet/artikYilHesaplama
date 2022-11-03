86fdd8aa3815b5a14a25351e8afb1e6d59693dfd
hidoyet <102352895+hidoyet@users.noreply.github.com>
2022-11-03 09:21:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,10 +8,20 @@ public class Main {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Yılı giriniz");
 		int year=scanner.nextInt();
-		if(year%4==0) {
-			System.out.println("Artık yıldır");
-		}else {
-			System.out.println("Artık yıl değildir");
+		if(year%100 !=0){
+			if(year%4==0) {
+				System.out.println("Artık yıldır");
+			}else {
+				System.out.println("Artık yıl değildir");
+			}
+		}
+		if(year%100==0){
+			if(year%400==0){
+				System.out.println("Artık yıldır");
+			}else {
+				System.out.println("Artık yıl değildir");
+			}
+		
 		}
 	}
 }
